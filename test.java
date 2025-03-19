1f1a860dcefdf24fe6cfc9b76d51460bf204e840
AlAminSarkerPrince <114096008+AlAminSarkerPrince@users.noreply.github.com>
2025-03-20 01:02:16 +0600

    Update test.java
    it will print hello world  
    again, it will do it 10 times.

--- test.java
+++ test.java
@@ -3,3 +3,10 @@ public class HelloWorld {
         System.out.println("Hello, world!");
     }
 }
+public class HelloWorld {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello, world!");
+        }
+    }
+}
